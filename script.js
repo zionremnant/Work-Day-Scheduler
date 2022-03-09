7234fd194bbf27a5cec54dbc69ecbcbe29d4ea25
Zion Yang <zionremnant@gmail.com>
2022-03-08 22:49:12 -0800

    js localstorage ea hr

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-var currentDayEl = $("#currentDay")
+$(document).ready(function(){
+    // current time & date
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss: a"));
+    // save button
+    $(".saveBtn").on("click", function(){
+        console.log(this);
+        var text = $(this).siblings(".description").value();
+        var time = $(this).parent().attribute("id");
+        localStorage.setItem(time, text);
+    })
+    $("#9am .description").value(localStorage.getItem("9am"));
+    $("#10am .description").value(localStorage.getItem("10am"));
+    $("#11am .description").value(localStorage.getItem("11am"));
+    $("#12pm .description").value(localStorage.getItem("12pm"));
+    $("#1pm .description").value(localStorage.getItem("1pm"));
+    $("#2pm .description").value(localStorage.getItem("2pm"));
+    $("#3pm .description").value(localStorage.getItem("3pm"));
+    $("#4pm .description").value(localStorage.getItem("4pm"));
+    $("#5pm .description").value(localStorage.getItem("5pm"));
 
+    function tracker (){
+        var currentHour = moment().hour();
 
-
-
-function displayDate(){
-    var rightNow = moment().format("dddd. MMMM Do. YYYY, h:mm:ss a");
-    currentDayEl.text(rightNow);
-
-    setInterval(() => {
-        displayDate()
-    }, 1000);
-}
+        $(".time-block").each(function(){
+            var 
+        })
+    }
+})
